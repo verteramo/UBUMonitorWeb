9f384f5cca9c5963d78926ec8a47d3f17a547290
Marcelo <49007909+verteramo@users.noreply.github.com>
2026-09-09 18:38:05 +0200

    Mejora de documentación.

--- app/src/app/pages/course-selection/course-selection.store.ts
+++ app/src/app/pages/course-selection/course-selection.store.ts
@@ -11,7 +11,9 @@ import {
   withState,
 } from '@ngrx/signals';
 
-/** Clasificaciones de las pestañas del componente. */
+/**
+ * Clasificaciones de las pestañas del componente.
+ */
 const classifications: CourseClassification[] = [
   'all',
   'starred',
@@ -21,48 +23,81 @@ const classifications: CourseClassification[] = [
   'past',
 ];
 
+/**
+ * Propiedades de estado del componente de selección de curso.
+ */
 type CourseSelectionState = {
   tab: number;
 };
 
+/**
+ * Tipo para el modelo del formulario.
+ */
 type CourseSelectionModel = {
   term: string;
   sorts: Sort[];
   courseId: number | null;
 };
 
+/**
+ * Estado inicial del componente.
+ */
 const initialState: CourseSelectionState = {
   tab: 0,
 };
 
+/**
+ * Estado inicial de la ordenación de las columnas.
+ */
 const initialSortsState: Sort[] = classifications.map(() => ({ active: '', direction: '' }));
 
+/**
+ * Estado inicial del modelo del formulario.
+ */
 const initialModelState: CourseSelectionModel = {
   term: '',
   sorts: initialSortsState,
   courseId: null,
 };
 
+/**
+ * Store de propiedades de estado del componente de selección de curso.
+ */
 export const CourseSelectionStore = signalStore(
   withState(initialState),
   withProps(({ tab }, service = inject(CourseService)) => ({
+    /**
+     * Recurso con los cursos.
+     */
     courses: rxResource({
       defaultValue: [],
       params: () => classifications[tab()],
       stream: ({ params }) => service.getCourses(params),
     }),
 
+    /**
+     * Modelo del formulario.
+     */
     model: signal(initialModelState),
   })),
   withComputed(({ model, tab }) => ({
+    /**
+     * Estado de la ordenación de las columnas.
+     */
     sort: computed(() => model().sorts[tab()]),
   })),
   withComputed(({ courses, model, sort }) => ({
+    /**
+     * Curso seleccionado.
+     */
     selectedCourse: computed(() => {
       const { courseId } = model();
       return courseId ? courses.value().find(({ id }) => id === courseId) : undefined;
     }),
 
+    /**
+     * Colección de cursos filtrados y ordenados.
+     */
     filteredCourses: computed(() => {
       const term = model().term.trim().toLowerCase();
 
@@ -84,11 +119,21 @@ export const CourseSelectionStore = signalStore(
     }),
   })),
   withMethods((store) => ({
+    /**
+     * Establece la pestaña seleccionada.
+     *
+     * @param tab Pestaña.
+     */
     setTab(tab: number): void {
       patchState(store, { tab });
     },
   })),
   withMethods(({ model, tab }) => ({
+    /**
+     * Establece el estado de ordenación de una columna determinada.
+     *
+     * @param newSort Nueva ordenación.
+     */
     setSort(newSort: Sort) {
       model.update((state) => ({
         ...state,
@@ -96,6 +141,11 @@ export const CourseSelectionStore = signalStore(
       }));
     },
 
+    /**
+     * Establece el ID del curso seleccionado.
+     *
+     * @param id ID del curso.
+     */
     setCourseId(id: number) {
       model.update((state) => ({ ...state, courseId: id }));
     },
